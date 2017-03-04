76d7304f0b0d55a476fcdfd9b78b970254b4df7f
Oleksii Moskal <mrdefender314@gmail.com>
2017-03-04 23:17:28 +0000

    sync

--- frontend/js/data.js
+++ frontend/js/data.js
@@ -9,6 +9,15 @@ window.userStore = {
   }
 };
 
+window.usersStore = {
+  data: [],
+  notifier: new EventEmitter(),
+  set: function(data) {
+    this.data = data;
+    this.notifier.emit('change', data);
+  }
+};
+
 window.groupStore = {
   data: [],
   notifier: new EventEmitter(),
