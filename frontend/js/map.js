4ace0e25c8c2087d4d9c0608201052412d8f6bd4
boromir1994 <boromir@e-mail.ua>
2017-03-04 21:05:18 +0200

    Have created sidebar and facebook button

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -1,32 +1,38 @@
-var map;
+  var map;
+  function initMap() {
+    var uluru = {lat: 50.27, lng: 30.30};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: uluru
+      });
 
-function initMap() {
-  var uluru = {lat: 50.27, lng: 30.30};
-  var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 8,
-      center: uluru
+
+    var contentString = '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h3 id="firstHeading" class="firstHeading">Borya</h3>'+
+        '<div id="bodyContent">'+
+        'body text'
+        '</div>'+
+        '</div>';
+
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString
     });
 
+    var image = 'img/oval-face.png';
+    var marker = new google.maps.Marker({
+      position: uluru,
+      icon: image,
+      map: map,
+      title: 'Uluru (Ayers Rock)'
+    });
 
-  var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h3 id="firstHeading" class="firstHeading">Borya</h3>'+
-      '<div id="bodyContent">'+
-      'body text'
-      '</div>'+
-      '</div>';
+    setTimeout(function() {
+      infowindow.open(map, marker);
+ }, 3000)
 
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
-
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-    title: 'Uluru (Ayers Rock)'
-  });
-  marker.addListener('click', function() {
-    infowindow.open(map, marker);
-  });
-}
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    });
+  }
