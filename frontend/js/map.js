51191bb71ef6c96b3df16ed560d8e2970867dfb4
boromir1994 <boromir@e-mail.ua>
2017-03-05 10:12:41 +0200

    i

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -4,17 +4,25 @@
 
   function renderMarkers (){
 
-  for (let i=0; i<users.length; i++){
-    if (users[i].infowindow){
-        infowindow.close();
+    for (let i=0; i<users.length; i++){
+      if (users[i].infowindow){
+          users[i].infowindow.close();
+      }
     }
-  }
 
+      if (window.stateStore.data.activeGroup.users[i].geo_location){
+        var location =  window.stateStore.data.activeGroup.users[i].geo_location.split(' ');
+
+        placeMarker({
+          lat:parseFloat(location[0]),
+          lng:parseFloat(location[1])
+        });
+      }
 
         for (var i in window.stateStore.data.activeGroup.users ){
 
-
           let array = window.stateStore.data.activeGroup.users[i].geo_location.split(' ');
+
           let contentString = '<div id="bubble">'+
               '<div id="bodyContent">'+
               window.stateStore.data.activeGroup.users[i].message+
@@ -107,6 +115,8 @@
         zoom: 8,
         center: uluru
       });
+
+
   }
 
 
