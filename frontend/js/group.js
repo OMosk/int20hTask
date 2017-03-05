56a3f8d6eecfff5cbb95de4a2d7759ec137e1d54
boromir1994 <boromir@e-mail.ua>
2017-03-05 07:14:11 +0200

    Initial commit

--- frontend/js/group.js
+++ frontend/js/group.js
@@ -34,51 +34,55 @@
       }
     }
 
-    // Select group and switch beetween them
-      $('.glyphicon-ok').on("click", function(){
-        if ($(this).hasClass('green')){
-          $(this).addClass("green");
-        }
-        else{
-          $('.glyphicon-ok').removeClass('green');
-          $(this).addClass("green");
-        }
-      });
-
-  $('ul.sidebar-nav li').on('click', function(){
-    var domElem = $(this).get(0);
-
-    for (let i in window.groupStore.data ){
-        if (domElem.getAttribute('data-id') ===  window.groupStore.data[i].group_id){
-          for (let j in  window.groupStore.data[i].users){
-
-          var sidebar = document.querySelector("ul.list-group");
-
-          while (sidebar.children.length !==0) {
-            sidebar.removeChild(sidebar.children[0]);
-          }
-
-          for (let j in window.groupStore.data[i].users ){
-              var li = document.createElement('li');
-              var li_class = document.createAttribute("class");
-              li_class.value = 'list-group-item';
-
-              li.setAttributeNode(li_class);
-
-              li.appendChild(document.createTextNode(window.groupStore.data[i].users[j].name));
-
-              var span = document.createElement("span");
-              var span_class = document.createAttribute('class');
-              span_class.value = 'glyphicon glyphicon-remove pull-right';
-              span.setAttributeNode(span_class);
-              li.appendChild(span);
-              sidebar.appendChild(li);
-            }
-          }
-        }
-      }
-  });
 
 groupStore.notifier.on('change', function(){
   renderSidebar();
+
+
+  // Select group and switch beetween them
+    $('.glyphicon-ok').on("click", function(){
+      console.log("her");
+      if ($(this).hasClass('green')){
+        $(this).addClass("green");
+      }
+      else{
+        $('.glyphicon-ok').removeClass('green');
+        $(this).addClass("green");
+      }
+    });
+
+$('ul.sidebar-nav li').on('click', function(){
+  var domElem = $(this).get(0);
+
+  for (let i in window.groupStore.data ){
+      if (domElem.getAttribute('data-id') ===  window.groupStore.data[i].group_id){
+        for (let j in  window.groupStore.data[i].users){
+
+        var sidebar = document.querySelector("ul.list-group");
+
+        while (sidebar.children.length !==0) {
+          sidebar.removeChild(sidebar.children[0]);
+        }
+
+        for (let j in window.groupStore.data[i].users ){
+            var li = document.createElement('li');
+            var li_class = document.createAttribute("class");
+            li_class.value = 'list-group-item';
+
+            li.setAttributeNode(li_class);
+
+            li.appendChild(document.createTextNode(window.groupStore.data[i].users[j].name));
+
+            var span = document.createElement("span");
+            var span_class = document.createAttribute('class');
+            span_class.value = 'glyphicon glyphicon-remove pull-right';
+            span.setAttributeNode(span_class);
+            li.appendChild(span);
+            sidebar.appendChild(li);
+          }
+        }
+      }
+    }
+});
+
 });
